08d6fae2e98a2745634b15ae3d4f517c048c5c06
Nabinkc10 <94090823+Nabinkc10@users.noreply.github.com>
2022-11-09 15:19:27 +0545

    create new .net core mvc project

--- src/WFHMS.Models/ViewModel/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WFHMS.Models.ViewModel
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
